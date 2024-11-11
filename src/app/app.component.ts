ea098d015f5c423891366d16a3d8cfae65fba1c9
xooxaa <37028661+xooxaa@users.noreply.github.com>
2024-11-11 12:34:55 +0100

    implement core game functionaliy add basic styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameService } from './game.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'fem-rock-paper-scissors';
+  playerChoice: string = '';
+  computerChoice: string = '';
+  result: string = '';
+  isChoosing: boolean = true;
+
+  constructor(private gameService: GameService) {}
+
+  play(choice: string): void {
+    this.playerChoice = choice;
+    this.computerChoice = this.gameService.getComputerChoice();
+    this.result = this.gameService.determineWinner(
+      this.playerChoice,
+      this.computerChoice
+    );
+    this.isChoosing = false;
+  }
+
+  resetGame(): void {
+    this.playerChoice = '';
+    this.computerChoice = '';
+    this.result = '';
+    this.isChoosing = true;
+  }
+
+  setGameMode(mode: 'RPS' | 'RPSLS'): void {
+    this.gameService.setGameMode(mode);
+  }
+
+  getGameMode(): string {
+    return this.gameService.gameMode();
+  }
+
+  getScore() {
+    return this.gameService.getScore();
+  }
 }
